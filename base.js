eb86e2100ff533658225d7f12eec85349f132f95
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-03-27 12:25:50 +0200

    aula 6 feita com sucesso

--- base.js
+++ base.js
@@ -19,7 +19,25 @@ const arquivos = [
     'Lista de compras.txt',
 ];
 
-const alfabeto = 'ABCDEFGHIJKLMNPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz @! 123456789 !';
+const cpfs =`
+
+ Os CPFs sao: 
+ 254.224.877-45 215.978.456-12 047.258.369-96
+
+ 963.987.321-00
+ `;
+ const ips = `
+
+  Os IPs sao:
+  0.0.0.0
+
+  192.168.0.25
+
+       10.10.5.12
+
+  255.255.255.255`;
+
+ const alfabeto = 'ABCDEFGHIJKLMNPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz @! 123456789 !';
 
 const html = '<p> Ola mundo </p> <p> Ola de novo </p> <div> Sou a div </div>';
 
@@ -29,5 +47,7 @@ module.exports = {
     texto,
     arquivos,
     html,
-    alfabeto
+    alfabeto,
+    cpfs,
+    ips
 }
