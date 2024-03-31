7e9845613b8afadd43f83f57b642fed7156922c6
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-03-31 21:52:37 +0200

    0peradores bitwise

---
+++ CBF/aula08.js
@@ -0,0 +1,20 @@
+/*                                  OPERADORES BITWISE                          */
+// exemplo 1
+let n1=11
+let=n2=14
+
+let res=n1&n2
+
+console.log(res)
+
+// exemplo 2
+let n3=10
+let n4=11
+let run=n3|n4
+console.log(run)
+
+//exemplo 3
+
+let n5=25
+let rui=n5 << 2
+console.log(rui)
