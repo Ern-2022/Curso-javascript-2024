bec72136672cdda20ed5dd1ff1a76048515ca31a
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-13 00:16:37 +0200

    craido aula 12 do curso javascript cfb cursos

---
+++ CBF/aula012.js
@@ -0,0 +1,5 @@
+const jogador1 ={nome:"Ernicio",energia:100,vida:3,magia:150}
+const jogador2 ={nome:"Ernicia",energia:100,vida:5,velocidade:80}
+const jogador3 = {...jogador1,...jogador2}
+
+console.log(jogador3)
