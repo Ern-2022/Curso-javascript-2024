40243031818e871ef6e80208fadbd41547329d87
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-13 10:53:25 +0200

    aula 13 feita com sucesso

---
+++ CBF/aula13.js
@@ -0,0 +1,26 @@
+// Aprendedo a usar a condicional IF e IF 
+
+// exemplo 1
+// let num = 40
+
+// if(num > 10){
+//     console.log('Numeral maior que 10')
+//     if(num > 50){
+//         console.log("Numeral maior que 50")
+//     }
+// }else if(num > 5){
+//     console.log("Numeral esta entre 6 e 10")
+// }else{
+//     console.log("Numeral menor ou igual a 5")
+// }
+
+// exemplo 2
+
+let energia = 100
+let clima = "chuva"
+if(energia > 70 && clima=="sol"){
+    console.log("Vou a praia!")
+}else{
+    console.log("Vou ao cinema!")
+}
+console.log('Fim do programa')
