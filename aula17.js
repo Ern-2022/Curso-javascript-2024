14be445a07dd96a6c9cce3245b10bba41cdc0805
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-13 22:44:03 +0200

    feito

---
+++ aula17.js
@@ -0,0 +1,17 @@
+// loop while
+
+5!=5*4*3*2*1==120
+
+let n=5
+let fat=1
+while(n>=1){
+    fat*=n
+    n--
+}
+
+console.log(fat)
+// let n = 0
+// while(true){
+//     console.log(n)
+//     n++
+// }
