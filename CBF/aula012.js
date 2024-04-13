34d0cf998282b0b6d28094c87cba53f261a8413d
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-13 08:18:38 +0200

    aula 12 feita

--- CBF/aula012.js
+++ CBF/aula012.js
@@ -2,5 +2,14 @@ const jogador1 ={nome:"Ernicio",energia:100,vida:3,magia:150}
 const jogador2 ={nome:"Ernicia",energia:100,vida:5,velocidade:80}
 const jogador3 = {...jogador1,...jogador2}
 
-// console.log(jogador3)
+//  console.log(jogador3)
 
+const objs1 = document.getElementsByTagName("div")
+const objs2 = [...document.getElementsByTagName("div")]
+
+console.log(objs1)
+console.log(objs2)
+
+objs2.forEach(element => {
+    console.log(element.innerHTML= "ernicio")
+});
