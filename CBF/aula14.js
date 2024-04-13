7e66f86332e70c85378882b3b9f693146387c736
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-13 13:46:53 +0200

    add

---
+++ CBF/aula14.js
@@ -0,0 +1,19 @@
+// na aula 14 vamos falar do comado switch case
+
+let colocacao = 5
+switch(colocacao){
+    case 1:
+        console.log("Primeiro lugar!")
+        break
+    case 2:
+        console.log("Segundo lugar!")
+        break
+    case 3:
+        console.log("Terceiro lugar!")
+        break
+    case 4:
+        console.log("Quarto lugar!")
+        break
+    default:
+        console.log("Nao subiu no podio")
+}
