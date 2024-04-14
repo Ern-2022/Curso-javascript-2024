a0e7718bf94f080ceb2a064790f8514c2056d51f
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-14 11:08:35 +0200

    aula 20 feita com sucesso

---
+++ CBF/aula20.js
@@ -0,0 +1,35 @@
+// APRENDEDO A USAR FUNCOES 
+
+// EXEMPLO 1
+
+// function nome(){
+//     console.log("CFB CURSOS")
+// }
+
+// for(let i=0;i<10; i++){
+//     nome()
+// }
+
+// EXEMPLO 2
+
+// function soma(){
+//     let n1=2
+//     let n2=13
+//     let soma = n1+n2
+//     console.log(soma)
+// }
+
+// for(let i=0; i<10; i++){
+//     soma()
+// }
+
+// EXEMPLO 3
+
+function mudarTexto(){
+    let d1 = document.getElementById("d1")
+    let d2 = document.getElementById("d2")
+    let d3 = document.getElementById("d3")
+    d1.innerHTML="curso ernicio"
+    d2.innerHTML="curso fina"
+    d3.innerHTML="curso python"
+}
