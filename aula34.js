bcb39413b68bff85d7b41be25e661df7e80014f6
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-15 23:46:18 +0200

    apredendo a usar o elemento addEventListener na aula 34

---
+++ aula34.js
@@ -0,0 +1,7 @@
+// A dicionando enentos com addEventListener
+// const c1 =document.getElementById("c1")
+const c1 =document.querySelector("#c1")
+const msg = ()=>{
+    alert('clicou')
+}
+c1.addEventListener("click",msg)
