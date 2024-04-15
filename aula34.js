746118491ca46b24494051fb7233c9012bed5c99
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-15 23:49:42 +0200

    add

--- aula34.js
+++ aula34.js
@@ -1,7 +1,12 @@
 // A dicionando enentos com addEventListener
 // const c1 =document.getElementById("c1")
 const c1 =document.querySelector("#c1")
-const msg = ()=>{
-    alert('clicou')
-}
-c1.addEventListener("click",msg)
+
+// const msg = ()=>{
+//     alert('clicou')
+// }
+
+
+c1.addEventListener("click",()=>{
+    alert('clicar')
+})
