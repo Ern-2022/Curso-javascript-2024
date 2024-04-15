7e91ec41c36d27f1e00796f07375d7774f7ea8cb
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-16 00:15:30 +0200

    aula 35 feita com sucesso

--- aula34.js
+++ aula34.js
@@ -1,12 +1,23 @@
 // A dicionando enentos com addEventListener
 // const c1 =document.getElementById("c1")
-const c1 =document.querySelector("#c1")
+// const c1 =document.querySelector("#c1")
+const cursosTodos = [...document.querySelectorAll('.curso')]
 
-const msg = ()=>{
-    alert('clicou')
-}
+cursosTodos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        const el = evt.target
+        el.classList.add("destaque") 
+        console.log(el.innerHTML + " foi clicado")   
+    })
 
-
-c1.addEventListener("click",(evt)=>{
-    console.log(evt.target)
 })
+
+// const msg = ()=>{
+//     alert('clicou')
+// }
+
+
+// c1.addEventListener("click",(evt)=>{
+//     const el = evt.target
+//     el.classList.add("destaque")
+// })
