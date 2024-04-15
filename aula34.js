9227339c8136c8268f07e7baac76a9d8307cad71
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-15 23:54:18 +0200

    add

--- aula34.js
+++ aula34.js
@@ -2,11 +2,11 @@
 // const c1 =document.getElementById("c1")
 const c1 =document.querySelector("#c1")
 
-// const msg = ()=>{
-//     alert('clicou')
-// }
+const msg = ()=>{
+    alert('clicou')
+}
 
 
-c1.addEventListener("click",()=>{
-    alert('clicar')
+c1.addEventListener("click",(evt)=>{
+    console.log(evt.target)
 })
