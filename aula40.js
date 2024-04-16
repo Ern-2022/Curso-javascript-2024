769e35bf27b65119a40bc85485bec0669ef76519
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-16 14:43:52 +0200

    aula 40 feita com sucesso

---
+++ aula40.js
@@ -0,0 +1,13 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector(".c1_2")
+const cursos = ["HTML", "CSS", "Javascript", "PHP", "React", "MySQL", "ReactNative"]
+
+cursos.map((el,chave) => {
+    const novoElement = document.createElement("div")
+    novoElement.setAttribute("id", "c"+chave)
+    novoElement.setAttribute("class", "curso c1")
+    novoElement.innerHTML = "ReactNative"
+    caixa1.appendChild(novoElement)
+})
+
