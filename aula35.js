6c1f75990f619b514bc9ffe5c17c033e6479a095
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-16 02:15:17 +0200

    aula 35 feita com sucesso

---
+++ aula35.js
@@ -0,0 +1,21 @@
+// praticado sobre addEventListener
+
+const caixa1 = document.querySelector("#caixa1")
+const caixa2 = document.querySelector("#caixa2")
+const btn = document.querySelector("#btn_copiar")
+const cursosTodos = [...document.querySelectorAll(".curso")]
+
+cursosTodos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        const curso = evt.target
+        curso.classList.toggle("selecionado")
+    })
+})
+
+btn.addEventListener("click",()=>{
+    const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
+    console.log(cursosSelecionados)
+    cursosSelecionados.map((el)=>{
+        caixa2.appendChild(el)
+    })
+})
