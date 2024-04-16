3d6a54379331619710aaecff9bbb374159bce03d
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-16 02:41:53 +0200

    aula 26 feita com sucesso

---
+++ aula36.js
@@ -0,0 +1,28 @@
+// praticado sobre addEventListener
+
+// praticado sobre addEventListener
+
+const caixa1 = document.querySelector("#caixa1")
+const caixa2 = document.querySelector("#caixa2")
+const btn_transferir = document.querySelector("#btn_transferir")
+const cursosTodos = [...document.querySelectorAll(".curso")]
+
+cursosTodos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        const curso = evt.target
+        curso.classList.toggle("selecionado")
+    })
+})
+
+btn_transferir.addEventListener("click",()=>{
+    const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
+    const cursosNaoSelecionados = [...document.querySelectorAll(".curso:not(.selecionado)")]
+    cursosSelecionados.map((el)=>{
+        caixa2.appendChild(el)
+    })
+
+    cursosNaoSelecionados.map((el)=>{
+        caixa1.appendChild(el)
+    })
+
+})
