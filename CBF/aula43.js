c1d77f3417eaeec9013ae0904b24bf71210be0b9
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-18 10:06:02 +0200

    add

--- CBF/aula43.js
+++ CBF/aula43.js
@@ -1,43 +1,43 @@
 //praticado o methodo filter no javascript
 
-const caixaCurso = document.querySelector("#caixaCursos")
+
+const caixaCursos = document.querySelector("#caixaCursos")
 const btn_c = [...document.querySelectorAll(".curso")]
 const c1_2 = document.querySelector("#c1_2")
 const cursos = ["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
-const btnCursoSelecionado = document.getElementById("btnCursoSelecionado")
+const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
 
 cursos.map((el,chave)=>{
-    const novoElemento = document.createElement("div")
+    const novoElemento=document.createElement("div")
     novoElemento.setAttribute("id","c"+chave)
     novoElemento.setAttribute("class","curso c1")
     novoElemento.innerHTML=el
 
-    const comando = document.createElement("div")
-    comando.setAttribute("class","comando")
+    const comandos = document.createElement("div")
+    comandos.setAttribute("class","comandos")
 
     const rb = document.createElement("input")
     rb.setAttribute("type","radio")
     rb.setAttribute("name","rb_curso")
 
-    comando.appendChild(rb)
-    novoElemento.appendChild(comando)
-    caixaCurso.appendChild(novoElemento)
+    comandos.appendChild(rb)
+    novoElemento.appendChild(comandos)
+    caixaCursos.appendChild(novoElemento)
 })
 
-
 btnCursoSelecionado.addEventListener("click",(evt)=>{
-    const todosRadios = [...document.querySelectorAll("input[type=radio]")]
-    let radioSelecionado=todosRadios.filter((ele,ind,arr)=>{
-        return ele.Checked
+    const todosRadios=[...document.querySelectorAll("input[type=radio]")]
+    let radiosSelecionado=todosRadios.filter((ele,ind,arr)=>{
+        return ele.checked
     })
-    radioSelecionado=radioSelecionado[0]
-    // const cursoSelecionado = radioSelecionado.parentNode.parentNode.firstChild.textContent
-    const cursoSelecionado = radioSelecionado.parentNode.previusSibling.textContent
-    alert("Curso selecionado: "+ cursoSelecionado)
-//     console.log(todosRadios)
-//     console.log(radioSelecionado)
-//     console.log(cursoSelecionado)
- })
+    radiosSelecionado=radiosSelecionado[0]
+    const cursoSelecionado=radiosSelecionado.parentNode.parentNode.textContent
+    // const cursoSelecionado=radiosSelecionado.parentNode.previusSibling.textContent
+    alert("Curso selecionado:" + cursoSelecionado)
+    // console.log(todosRadios)
+    // console.log(radiosSelecionado)
+    // console.log(cursoSelecionado)
+})
 
 // parentNode
 // childNode[nodenumber]
--- aula44.js
+++ aula44.js
@@ -3,12 +3,12 @@ const caixaCursos = document.querySelector("#caixaCursos")
 const btn_c = [...document.querySelectorAll(".curso")]
 const c1_2 = document.querySelector("#c1_2")
 const cursos = ["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
-const btnCursoSelecionado = document.getElementById("btnCursoSelecionado")
+const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
 
 cursos.map((el,chave)=>{
     const novoElemento=document.createElement("div")
     novoElemento.setAttribute("id","c"+chave)
-    novoElemento.getAttribute("class","curso c1")
+    novoElemento.setAttribute("class","curso c1")
     novoElemento.innerHTML=el
 
     const comandos = document.createElement("div")
@@ -19,10 +19,20 @@ cursos.map((el,chave)=>{
     rb.setAttribute("name","rb_curso")
 
     comandos.appendChild(rb)
-
     novoElemento.appendChild(comandos)
-
     caixaCursos.appendChild(novoElemento)
+})
 
-
+btnCursoSelecionado.addEventListener("click",(evt)=>{
+    const todosRadios=[...document.querySelectorAll("input[type=radio]")]
+    let radiosSelecionado=todosRadios.filter((ele,ind,arr)=>{
+        return ele.checked
+    })
+    radiosSelecionado=radiosSelecionado[0]
+    const cursoSelecionado=radiosSelecionado.parentNode.parentNode.textContent
+    // const cursoSelecionado=radiosSelecionado.parentNode.previusSibling.textContent
+    alert("Curso selecionado:" + cursoSelecionado)
+    // console.log(todosRadios)
+    // console.log(radiosSelecionado)
+    // console.log(cursoSelecionado)
 })
