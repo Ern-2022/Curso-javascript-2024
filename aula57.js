171e4a323b2cfd03681494d50e47881878afb660
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-22 22:47:17 +0200

    AULA 57 FEITA COM SUCESSO

--- aula57.js
+++ aula57.js
@@ -1,10 +1,14 @@
 // Aprendendo sobre obecto no javascript
 
 class pessoa{
-    constructor(){
-        this.nome = "ern"
+    constructor(ERN){
+        this.nome = ERN
     }
 }
 
-let p1 = new pessoa()
+let p1 = new pessoa(10)
+let p2 = new pessoa("ERN")
+let p3 = new pessoa("EULER")
 console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
