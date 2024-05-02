283ef737b393f4fdd7fd4a9f80917ccac556de1c
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-02 04:02:31 +0200

    lessson 74 done sucessful

---
+++ CBF/aula74.js
@@ -0,0 +1,85 @@
+// criando aba em gaveta no javascript
+const teclasNum = [...document.querySelectorAll(".num")]
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector(".res")
+const display = document.querySelector(".display")
+const ton = document.getElementById("ton")
+const tlimpar = document.getElementById("tlimpar")
+const tigual = document.getElementById("tigual")
+const tcpy = document.getElementById("tcpy")
+const teste = document.getElementById("teste")
+const calc_aba = document.getElementById("calc_aba")
+const calc = document.getElementById("calc")
+const span_calc_aba = [...document.querySelectorAll(".span_calc_aba")]
+
+let sinal = false
+let decimal = false
+
+teclasNum.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        sinal = false
+        if (evt.target.innerHTML == ",") {
+            if (!decimal) {
+                decimal = true
+                if (display.innerHTML == "0") {
+                    display.innerHTML = "0,"
+                } else {
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }
+        } else {
+            if (display.innerHTML == "0") {
+                display.innerHTML = ""
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
+    })
+})
+
+teclasOp.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        if (!sinal) {
+            sinal = true
+            if (display.innerHTML == "0") {
+                display.innerHTML = ""
+            }
+            if (evt.target.innerHTML == "x") {
+                display.innerHTML += "*"
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }
+        }
+    })
+})
+
+tlimpar.addEventListener("click", (evt) => {
+    sinal = false
+    decimal = false
+    display.innerHTML = "0"
+})
+
+tigual.addEventListener("click", (evt) => {
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
+})
+
+tcpy.addEventListener("click", (evt) => {
+    navigator.clipboard.writeText(display.innerHTML)
+    // teste.select()
+    // teste.setSelectionRange(0,99999)//ser no mobile
+    // navigator.clipboard.writeText(teste.value)
+})
+
+calc_aba.addEventListener("click", (evt) => {
+    calc.classList.toggle("calc_exibir")
+    if (evt.target.innerHTML == "▶") {
+        return calc_aba.innerHTML = "◀"
+    } else {
+        return calc_aba.innerHTML = "▶"
+
+    }
+})
+
+
