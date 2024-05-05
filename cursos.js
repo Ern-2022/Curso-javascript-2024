aec9983238767361a71c75cdf1e76cacad6f563e
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-05 19:00:38 +0200

    Lesson 92 done sucessfully

---
+++ cursos.js
@@ -0,0 +1,4 @@
+const cursos = ["Javascript","HTML","CSS","Arduino","Raspherry","C++","Python","Java","C#"]
+const carros = ["polo","T-cross","Golf","Onix","Cruse","Argo","Cronos"]
+
+export{cursos,carros}
