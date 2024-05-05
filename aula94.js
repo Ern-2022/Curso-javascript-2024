89362acfc604fbc9d6ca6b7589bdc4ad5a0f9daf
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-05 22:09:10 +0200

    add

--- aula94.js
+++ aula94.js
@@ -1,7 +1,7 @@
-// import getTodosCursos ,{ cursos} from "./cursos3.js";
-import getTodosCursos, { cursos } from "./cursos3.js"
-// import { cursos as c, getCursos as gc} from "./cursos3.js"
-import * as m_cursos from "./cursos3.js"
+// import getTodosCursos ,{ cursos} from "./cursos4.js";
+import getTodosCursos, { cursos } from "./cursos4.js"
+// import { cursos as c, getCursos as gc} from "./cursos4.js"
+import * as m_cursos from "./cursos4.js"
 
 console.log(m_cursos.cursos)
 console.log(m_cursos.getCursos(5))
---
+++ aula95.js
@@ -0,0 +1,5 @@
+import c from "./cursos4.js";
+
+c.addcurso("Photoshop")
+c.apagarCursos()
+console.log(c.getTodosCursos())
