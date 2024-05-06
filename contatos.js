552bd8b6fbb4cb4d40e19d35d2979384ec3f0a9e
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-06 06:28:01 +0200

    ADD

--- contatos.js
+++ contatos.js
@@ -14,19 +14,23 @@ let contato ={
             email:novoContato.email
         }
 
-        contatos.push({})
-        const div =document.createElement("div")
-        div.setAttribute("class","contato")
-        const p_nome = document.createElement("p")
-        p_nome.innerHTML=novoContato.nome
-        const p_telefone = document.createElement("p")
-        p_telefone.innerHTML=novoContato.telefone
-        const p_mail = document.createElement("p")
-        p_mail.innerHTML=novoContato.email
-        div.appendChild(p_nome)
-        div.appendChild(p_telefone)
-        div.appendChild(p_mail)
-        destinoDom.appendChild(div)
+        contatos.push(cont)
+        destinoDom.innerHTML=""
+        contatos.forEach((c)=>{
+            const div = document.createElement("div")
+            div.setAttribute("class", "contato")
+            const p_nome = document.createElement("p")
+            p_nome.innerHTML = c.nome
+            const p_telefone = document.createElement("p")
+            p_telefone.innerHTML = c.telefone
+            const p_mail = document.createElement("p")
+            p_mail.innerHTML = c.email
+            div.appendChild(p_nome)
+            div.appendChild(p_telefone)
+            div.appendChild(p_mail)
+            destinoDom.appendChild(div)
+
+        })
     }
 }
 
