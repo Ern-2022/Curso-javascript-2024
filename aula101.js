2f4a82e718c6ff3d5a9d2acf48beab886442505d
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-07 05:53:27 +0200

    AULA FEItA COM SUCESSO

---
+++ aula101.js
@@ -0,0 +1,14 @@
+// funcoes para manipukacao de String
+
+let nome = new String("ERNICO INACIO IBODEIRO")
+let canal = new String("CFBcursos")
+
+console.log(nome)
+console.log(typeof(nome))
+console.log(nome.charAt(0))
+console.log(nome.charAt(5))
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(canal))
+console.log(nome.indexOf("I"))
+console.log(nome.lastIndexOf("I"))
