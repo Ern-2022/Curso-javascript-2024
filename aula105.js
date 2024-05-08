c5602d184dc7180c78fc55311039ac5b7c1e4414
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-08 20:09:42 +0200

    lesson 105 done  sucessfully

---
+++ aula105.js
@@ -0,0 +1,8 @@
+// APRENDENDO SOBRE EXPRESSOES REGULARES
+
+let nome = new String("Witimane Chilundo Ferreira")
+
+console.log(nome.search(/ferreira/i));
+
+console.log(nome.match(/witimane/ig));
+console.log(nome.replace(/chilundo/ig,"Nyusi"));
