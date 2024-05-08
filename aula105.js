aa91634be5aa5729d55c275c26386854d80a04d9
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-08 20:15:27 +0200

    add

--- aula105.js
+++ aula105.js
@@ -2,7 +2,7 @@
 
 let nome = new String("Witimane Chilundo Ferreira")
 
+console.log(nome);
 console.log(nome.search(/ferreira/i));
-
-console.log(nome.match(/witimane/ig));
+console.log(nome.match(/I/ig));
 console.log(nome.replace(/chilundo/ig,"Nyusi"));
