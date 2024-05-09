bb59192dacfe0a9b3597a6265102576b99dc4ad8
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-09 18:15:18 +0200

    lesson 114 done sucessful

---
+++ aula114.js
@@ -0,0 +1,12 @@
+// Aprendendo sobre desestruturacao em javascript
+
+let numeros = ()=>{
+    return [10,20,30,40]
+}
+
+let [a,b,c,d] = numeros()
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
