fe63ac620d3c5a85079749d6466691bcaacf921d
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-10 10:44:34 +0200

    add

---
+++ aula118.js
@@ -0,0 +1,35 @@
+// let i =null
+// for(i=0;i<10;i++);
+// console.log(i)
+
+// let i = null; i=0
+// console.log(i)
+
+// [,(  quando comecamos com conchetes | parenteses temos que colocar ponto e virgula na linha anterior
+
+// let canal = "cfbcursos";
+// ["a","b","f","v","l","p","o","u"].forEach(l => console.log(l));
+
+// let n1 =[1,2]
+// let n2 =[3,4];
+// [n1,n2].map(v=>console.log(v))
+
+// let n = null;
+// (n=15)
+// console.log(n)
+
+// let numeros = [10,34,21]
+// let [n1,n2,n3]= numeros
+
+// console.log(n1)
+// console.log(n2)
+// console.log(n3)
+
+
+let num = {n1:30,n2:60,n3:90}
+
+let {n1,n2,n3}=num
+
+console.log(n1)
+console.log(n2)
+console.log(n3)
