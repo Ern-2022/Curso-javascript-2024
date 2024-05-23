3d66340d13e6bc858096f0e11cd1276358f10241
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-05-23 23:54:19 +0200

    add

--- cxmsg.js
+++ cxmsg.js
@@ -12,8 +12,23 @@ class Cxmsg {
         this. destino = document.body
     }
     mostrar = ()=>{
-        console.log(this.texto)
+        this.divmsg=document.createElement("div")
+        const estilo_divmsg =
+        "display:flex;"+
+        "justify-content:center;"+
+        "align-items:center;"+
+        "top:0px;"+
+        "left:0px;"+
+        "width:100%;"+
+        "height:100vh;"+
+        "position:absolute;"+
+        "background-color:rbga(0,0,0,0.7);"
+
+        this.divmsg.setAttribute("id","divmsg")
+        this.divmsg.setAttribute("style",estilo_divmsg)
+        this.destino.prepend(this.divmsg)
     }
+    
 
     ocultar = ()=>{
 
--- topo.js
+++ topo.js
@@ -3,12 +3,12 @@ const topo = document.createElement("div")
 
 const estilotopo =
     "display: flex;" +
-    "justify - content: space - between;" +
-    "background - color: #00f;" +
-    "align - items: center;"
+    "justify-content: space-between;" +
+    "background-color: #00f;" +
+    "align-items: center;"
 
 
-const estilo_img_logo = "width:200px;"
+const estilo_img_logo = "width:200px;"+
 
 
 topo.setAttribute("id", "topo")
@@ -19,8 +19,6 @@ const logo =
     " <div id='logo' class='logo'>" +
     " <img src='logo.svg' title='kosmo' style='"+estilo_img_logo+"'>" +
     "</div>"
-
-topo.innerHTML += logo
 
 const login =
     " <div id='login' class='login'>" +
