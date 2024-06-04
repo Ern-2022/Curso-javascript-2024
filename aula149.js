d13156787c1be53b5d20b50cb4715611b2e0361d
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-04 23:50:28 +0200

    lesoon 1

--- aula149.js
+++ aula149.js
@@ -1,12 +1,20 @@
+import { Cxmsg } from "./cxmsg.js";
 import { Login } from "./login149.js";
 
 
 const callback_ok = ()=>{
-    alert("ok");
+    
 }
 
 const callback_naook = ()=>{
-    alert("nao ok")
-}
+    const config={
+        cor : "#800",
+        tipo: "ok",
+        testos: null,
+        comando_sn:null,
+    }
+    Cxmsg.mostrar(config,"Erro","login nao efetuado! Usuario / senha incorretos.")
+
+}    
 
 Login.login(callback_ok,callback_naook);
