c65c1940a1ff1762897dded63d6502205db02423
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-06 10:40:22 +0200

    lesson 155 done sucessfully

--- aula155.js
+++ aula155.js
@@ -4,6 +4,7 @@ const configdgv = {
 }
 const dgv = (configdgv) => {
     const dgvDados = document.getElementById(configdgv.idDestino)
+    dgvDados.innerHTML= ""
     fetch(configdgv.endpoind)
         .then(res => res.json())
         .then(res => {
@@ -12,30 +13,43 @@ const dgv = (configdgv) => {
                 dgvLinha.setAttribute("class", "dgvLinha")
 
                 const c1 = document.createElement("div")
-                c1.setAttribute("class", "c1")
+                c1.setAttribute("class", "coluna c1")
                 c1.innerHTML = el.id
                 dgvLinha.appendChild(c1)
 
                 const c2 = document.createElement("div")
-                c2.setAttribute("class", "c2")
+                c2.setAttribute("class", "coluna c2")
                 c2.innerHTML = el.produto
                 dgvLinha.appendChild(c2)
 
                 const c3 = document.createElement("div")
-                c3.setAttribute("class", "c3")
+                c3.setAttribute("class", "coluna c3")
                 c3.innerHTML = el.marca
                 dgvLinha.appendChild(c3)
 
                 const c4 = document.createElement("div")
-                c4.setAttribute("class", "c4")
+                c4.setAttribute("class", "coluna c4")
                 c4.innerHTML = el.modelo
                 dgvLinha.appendChild(c4)
 
                 const c5 = document.createElement("div")
-                c5.setAttribute("class", "c5")
-                c5.innerHTML = "D E V"
+                c5.setAttribute("class", "coluna c5")
                 dgvLinha.appendChild(c5)
 
+                const imgTrash = document.createElement("img")
+                imgTrash.setAttribute("class","dgvIcone")
+                imgTrash.setAttribute("src","trash.svg")
+                c5.appendChild(imgTrash)
+
+                const imgEdit = document.createElement("img")
+                imgEdit.setAttribute("class","dgvIcone")
+                imgEdit.setAttribute("src","edit.svg")
+                c5.appendChild(imgEdit)
+
+                const imgVisibility = document.createElement("img")
+                imgVisibility.setAttribute("class","dgvIcone")
+                imgVisibility.setAttribute("src","visibility.svg")
+                c5.appendChild(imgVisibility)
                 dgvDados.appendChild(dgvLinha)
             });
 
