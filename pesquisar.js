d3a7220de3d00dd337e3a465399a80ec6cbf48f4
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-09 17:28:40 +0200

    loadind

--- pesquisar.js
+++ pesquisar.js
@@ -2,10 +2,20 @@ const btn_pesq = document.getElementById("btn_pesq")
 const f_textpesq = document.getElementById("f_textpesq")
 
 btn_pesq.addEventListener("click",(evt)=>{
-    if(f_textpesq.value==""){
+    const valorpesq = f_textpesq.value
+    if (valorpesq==""){
         alert("Digite a pesquisa")
         f_textpesq.focus()
         return
     }
-    console.log(document.getElementsByName("f_pesq").value);
+    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value
+    if(f_pesq=="id"){
+
+    }else if(f_pesq=="nome"){
+
+    }else if(f_pesq=="nasc"){
+
+    }else if(f_pesq=="email"){
+
+    }
 })
