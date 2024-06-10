4571b86bd2837d2ed3ee2e5ade5a1ec2c785f02a
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-10 23:11:01 +0200

    add

--- gestao.js
+++ gestao.js
@@ -2,11 +2,11 @@ const dados = document.getElementById("dados")
 const fundopopup = document.getElementById("fundopopup")
 const btn_gravar = document.getElementById("btn_gravar")
 const btn_cancelar = document.getElementById("btn_cancelar")
-const f_id
-const f_nome
-const f_celular
-const f_email
-const f_dtnasc
+const f_id = document.getElementById("f_id")
+const f_nome = document.getElementById("f_nome")
+const f_celular = document.getElementById("f_celular")
+const f_email = document.getElementById("f_email")
+const f_dtnasc = document.getElementById("f_dtnasc")
 
 
 btn_gravar.addEventListener("click", (evt) => {
