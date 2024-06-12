b50652a08e243a54938981931d23f2ed0dafbf89
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-13 00:46:31 +0200

    add

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const btn_menuPrincipal = document.getElementById("btn_menuPrincipal")
 const menuPrincipal = document.getElementById("menuPrincipal")
+const todosmenusprincipais = [...document.querySelectorAll(".menuItem")]
 
 btn_menuPrincipal.addEventListener("click", (evt) => {
     menuPrincipal.classList.toggle("ocultar")
 })
+
+todosmenusprincipais.forEach(e => {
+    e.addEventListener("click", (evt) => {
+        menuPrincipal.classList.add("ocultar")
+    })
+});
