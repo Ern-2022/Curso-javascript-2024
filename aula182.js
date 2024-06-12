40aee735fac352a8376a5f8194ac6e81310bbed2
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-12 13:38:32 +0200

    lesson 183 done sucessfully

--- aula182.js
+++
@@ -1,7 +0,0 @@
-const btn_menuPrincipal = document.getElementById("btn_menuPrincipal")
-const menuPrincipal = document.getElementById("menuPrincipal")
-const todosmenusprincipais = document.getElementById("btn_menuItem")
-
-btn_menuPrincipal.addEventListener("click",(evt)=>{
-    menuPrincipal.classList.toggle("ocultar");
-})
---
+++ index.js
@@ -0,0 +1,6 @@
+const btn_menuPrincipal = document.getElementById("btn_menuPrincipal")
+const menuPrincipa = document.getElementById("menuPrincipa")
+
+btn_menuPrincipal.addEventListener("click",(evt)=>{
+.classList.toggle("ocultar")
+})
