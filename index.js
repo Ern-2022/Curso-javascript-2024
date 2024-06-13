ada08a8b04ffb0d5ad3ca007615dfdabd0d1fa7c
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-13 14:08:24 +0200

    Lesson 188 done sucessfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn_menuPrincipal = document.getElementById("btn_menuPrincipal")
 const menuPrincipal = document.getElementById("menuPrincipal")
-const todosmenusprincipais = [...document.querySelectorAll(".menuItem")]
+const todosmenusprincipais = [...document.querySelectorAll(".btn_menuItem")]
 
 btn_menuPrincipal.addEventListener("click", (evt) => {
     menuPrincipal.classList.toggle("ocultar")
---
+++ kosmo.js
@@ -0,0 +1,41 @@
+const dadosGrid = document.getElementById("dadosGrid")
+
+const endpoint = "http://127.0.0.1:1880/todosusuarios"
+
+fetch(endpoint)
+.then(res=>res.json())
+.then(res=>{
+    console.log(res)
+    dadosGrid.innerHTML=""
+    res.forEach(e=> {
+        const divlinha =document.createElement("div")
+        divlinha.setAttribute("class","linhaGrid")
+        dadosGrid.appendChild(divlinha)
+        
+        const divc1 =document.createElement("div")
+        divc1.setAttribute("class","colunaLinhaGrid c1")
+        divc1.innerHTML=e.n_usuario_usuario
+        divlinha.appendChild(divc1)
+
+        const divc2 =document.createElement("div")
+        divc2.setAttribute("class","colunaLinhaGrid c2")
+        divc2.innerHTML=e.s_nome_usuario
+        divlinha.appendChild(divc2)
+
+        const divc3 =document.createElement("div")
+        divc3.setAttribute("class","colunaLinhaGrid c3")
+        divc3.innerHTML=e.c_tipo_usuario
+        divlinha.appendChild(divc3)
+
+        const divc4 =document.createElement("div")
+        divc4.setAttribute("class","colunaLinhaGrid c4")
+        divc4.innerHTML=e.c_status_usuario
+        divlinha.appendChild(divc4)
+
+        const divc5 =document.createElement("div")
+        divc5.setAttribute("class","colunaLinhaGrid c5")
+        divlinha.appendChild(divc5)
+
+    });
+})
+
